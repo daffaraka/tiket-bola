8aab5e31eb1f21f1233ecb6a243b4ced087fa96a
Satomo <satriyo@ymail.com>
2023-08-12 21:47:10 +0700

    add filter date and search | add loading/empty state

---
+++ const.js
@@ -0,0 +1,29 @@
+const DATA = [
+   {
+      id: 1,
+      slug: '1-psm-persib',
+      title: 'PSM VS PERSIB',
+      date: '2023-12-08',
+      place: 'Stadion Geleora B. J. Habibie (GBH)',
+      price: 80000,
+      thumbnail: 'image/Group 138.png'
+   },
+   {
+      id: 2,
+      slug: '1-persebaya-persija',
+      title: 'Persebaya VS PERSIJA',
+      date: '2023-12-08',
+      place: 'Stadion Geleora B. J. Habibie (GBH)',
+      price: 80000,
+      thumbnail: 'image/Group 33543.png'
+   },
+   {
+      id: 3,
+      slug: '3-psis-pss',
+      title: 'PSIS VS PSS',
+      date: '2023-12-10',
+      place: 'Stadion Geleora B. J. Habibie (GBH)',
+      price: 80000,
+      thumbnail: 'image/Group 33545.png'
+   },
+]
